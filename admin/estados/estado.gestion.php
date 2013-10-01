21d4d9eff9ffeed12ebc0a13a5dd150822a42007
ariel <ariel_nb_011@hotmail.com>
2013-10-01 18:38:05 +0200

    controlaar estados postergar ,prorroga

--- admin/estados/estado.gestion.php
+++ admin/estados/estado.gestion.php
@@ -72,7 +72,7 @@ try {
  
     $fechafin=$vigencia->fecha_fin;
 
-    echo date("Y-m-d",strtotime("$fechafin +6 month") );;
+    echo date("Y-m-d",strtotime("$fechafin +6 month") );;;
  $vigencia->fecha_fin=  date("d/m/Y",strtotime("$fechafin +12 month") );
      
      $vigencia->estado_vigencia='PO';
