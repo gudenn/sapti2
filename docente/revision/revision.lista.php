1b4a2efcc8121a0b3889ebe6941dc89b2cdf9e35
Sesai Quispe <sesaiquispe@gmail.com>
2013-10-17 11:43:28 -0400

    revisiones corregido

--- docente/revision/revision.lista.php
+++ docente/revision/revision.lista.php
@@ -15,6 +15,7 @@ try {
 
   $CSS[]  = URL_CSS . "academic/tables.css";
   $CSS[]  = URL_CSS . "editablegrid.css";
+  $CSS[]  = URL_JS . "ventanasmodales/simplemodaldetalle.css";
   $smarty->assign('CSS',$CSS);
 
   //JS
@@ -22,6 +23,7 @@ try {
   $JS[]  = URL_JS . "tablaeditable/editablegrid-2.0.1.js";
   $JS[]  = URL_JS . "tablaeditable/tabla.revision.lista.js";
   $JS[]  = URL_JS . "ventanasmodales/observacion.detalle.js";
+  $JS[]  = URL_JS . "ventanasmodales/jquery.simplemodal-1.4.4.js";
   $smarty->assign('JS',$JS);
   
   if (isset($_GET['id_estudiante'])) 
