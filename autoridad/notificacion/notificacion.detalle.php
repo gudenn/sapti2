34213cda3a5e8e5a5ef0b9cb0df6005ef6eae4df
Alavi <alaviserafin@gmail.com>
2013-10-19 06:32:04 -0400

    Detalle

--- autoridad/notificacion/notificacion.detalle.php
+++ autoridad/notificacion/notificacion.detalle.php
@@ -42,6 +42,7 @@ try {
   leerClase('Notificacion');
   leerClase('Estudiante');
   leerClase('Proyecto');
+  leerClase('Tribunal');
   
 
   //Sexo del usuario
@@ -53,11 +54,21 @@ try {
   if (isset($_GET['notificacion_id']) && is_numeric($_GET['notificacion_id']))
   {
     
+      $smarty->assign('accion', array(
+     Tribunal::ACCION_AC =>  Tribunal::ACCION_AC,
+     Tribunal::ACCION_RE =>  Tribunal::ACCION_RE
+          
+              ));
+    
     $notificacion  = new Notificacion($_GET['notificacion_id']);
     $proyecto      =  new Proyecto($notificacion->proyecto_id);
     $estudiante   =  $proyecto->getEstudiante();
     
      //echo $_GET['notificacion_id'];
+    //$notificacion
+    $tipo= Notificacion::TIPO_ASIGNACION;
+   // echo $tipo;
+    $smarty->assign("tiponotificacion",$tipo);
     
      $smarty->assign("notificacion", $notificacion);
      $smarty->assign("proyecto", $proyecto);
@@ -65,21 +76,155 @@ try {
   }
   
 
-  //tutor
-  $id = '';
-  if (isset($_GET['tutor_id']) && is_numeric($_GET['tutor_id']) )
-    $id = $_GET['tutor_id'];
-  $tutor   = new Tutor($id);
-  $usuario = new Usuario($tutor->usuario_id);
-
   if (isset($_POST['tarea']) && $_POST['tarea'] == 'registrar' && isset($_POST['token']) && $_SESSION['register'] == $_POST['token'])
   {
     
     $EXITO = false;
     mysql_query("BEGIN");
+    if(isset($_POST['id_notificacion']))
+    {
+      
+      $notificacion = new Notificacion($_POST['id_notificacion']);
+     $tribunal=    $notificacion->getNotificacionTribunal(getSessionUser()->id);
+     
+     $proyecto= new Proyecto($notificacion->proyecto_id);
+     
+    echo  $tribunal->id;
+     
+     if($tribunal)
+     {
+        
+       $idtribuanl=$tribunal->id;
+      $query = "UPDATE notificacion_tribunal nt SET nt.estado_notificacion='V'  WHERE nt.tribunal_id=$idtribuanl";
+       //  mysql_query($query);
+         
+    if( $_POST['accion']==Tribunal::ACCION_AC)
+     {
+      
+      
+      
+      // $tribunal = new Tribunal($_POST['ids']);
+       $tribunal->visto=  Tribunal::VISTO;
+       $tribunal->accion=$_POST['accion'];
+       $tribunal->detalle=$_POST['detalle'];
     
-    // OPCIONES
+       
+       
+     $notificacions= new Notificacion();
+     $notificacions->objBuidFromPost();
+     $notificacions->proyecto_id=$proyecto->id; 
+     $notificacions->tipo=  Notificacion::TIPO_MENSAJE;
+     $notificacions->fecha_envio= date("j/n/Y");
+     $notificacions->asunto="Asignacion de Tribunales";
+    // $notificacions->detalle=;
+     $notificacions->prioridad=5;
+     $notificacions->estado = Objectbase::STATUS_AC;
 
+    $noticaciones= array('estudiantes'=>array($proyecto->getEstudiante()->id));
+  $notificacions->enviarNotificaion( $noticaciones);
+    
+    $tribunal->save();
+  
+     }else
+    {
+      
+          $tribunal->visto=  Tribunal::VISTO;
+          $tribunal->accion=$_POST['accion'];
+          $tribunal->detalle=$_POST['detalle'];
+      // $tribunal->estado= Objectbase::STATUS_DE;
+       
+        $notificacions= new Notificacion();
+        $notificacions->objBuidFromPost();
+        $notificacions->proyecto_id=$proyecto->id; 
+        $notificacions->tipo=  Notificacion::TIPO_MENSAJE;
+        $notificacions->fecha_envio= date("j/n/Y");
+        $notificacions->asunto="Asignacion de Tribunales";
+        $notificacions->detalle=$_POST['detalle'];
+        $notificacions->prioridad=5;
+        $notificacions->estado = Objectbase::STATUS_AC;
+
+       $noticaciones= array('estudiantes'=>array($proyecto->getEstudiante()->id));
+      $notificacions->enviarNotificaion( $noticaciones);
+        
+        
+       $tribunal->save();
+    }
+    
+       
+       
+       
+       
+       
+     }else{
+       
+       
+       
+     $proyectotutor=  $notificacion->getProyectoTutor(getSessionUser()->id);
+       
+       
+              $idtribuanl=$tribunal->id;
+      $query = "UPDATE notificacion_tutor nt SET nt.estado_notificacion='V'  WHERE nt.notificacion_id=$notificacion->id";
+       //  mysql_query($query);
+         
+    if( $_POST['accion']==Tribunal::ACCION_AC)
+     {
+      
+      /**
+       *  const ACEPTADO   = "AC";
+        const RECHADO    = "RE";
+       */
+      
+      // $tribunal = new Tribunal($_POST['ids']);
+      $proyectotutor->estado_tutoria=  Proyecto_tutor::ACEPTADO;
+      
+       
+     $notificacions= new Notificacion();
+     $notificacions->objBuidFromPost();
+     $notificacions->proyecto_id=$proyecto->id; 
+     $notificacions->tipo=  Notificacion::TIPO_MENSAJE;
+     $notificacions->fecha_envio= date("j/n/Y");
+     $notificacions->asunto="Nombramiento de tutor";
+    // $notificacions->detalle=;
+     $notificacions->prioridad=5;
+     $notificacions->estado = Objectbase::STATUS_AC;
+
+    $noticaciones= array('estudiantes'=>array($proyecto->getEstudiante()->id));
+  $notificacions->enviarNotificaion( $noticaciones);
+    
+    $proyectotutor->save();
+  
+     }else
+    {
+       $proyectotutor->estado_tutoria=  Proyecto_tutor::RECHADO;
+    
+       
+        $notificacions= new Notificacion();
+        $notificacions->objBuidFromPost();
+        $notificacions->proyecto_id=$proyecto->id; 
+        $notificacions->tipo=  Notificacion::TIPO_MENSAJE;
+        $notificacions->fecha_envio= date("j/n/Y");
+        $notificacions->asunto= "Nombramiento de tutor";
+        $notificacions->detalle=$_POST['detalle'];
+        $notificacions->prioridad=5;
+        $notificacions->estado = Objectbase::STATUS_AC;
+
+       $noticaciones= array('estudiantes'=>array($proyecto->getEstudiante()->id));
+      $notificacions->enviarNotificaion( $noticaciones);
+        
+        
+       $proyectotutor->save();
+    }
+       
+      
+       
+  
+     }
+      
+      
+      
+    }
+    
+   
     $EXITO = TRUE;
     mysql_query("COMMIT");
   }
--- docente/notificacion/notificacion.detalle.php
+++ docente/notificacion/notificacion.detalle.php
@@ -10,7 +10,7 @@
   $menuList[]     = array('url'=>URL . Docente::URL , 'name'=>'Docente');
   $menuList[]     = array('url'=>URL . Docente::URL . 'notificacion/','name'=>'Notificaciones');
   $menuList[]     = array('url'=>URL . Docente::URL . 'notificacion/notificacion.gestion.php','name'=>'Archivo de Notificaiones');
-  $menuList[]     = array('url'=>URL . EstDocenteudiante::URL . 'notificacion/notificacion.detalle.php','name'=>'Detalle de Notificaciones');
+  $menuList[]     = array('url'=>URL . Docente::URL . 'notificacion/notificacion.detalle.php','name'=>'Detalle de Notificaciones');
   $url_base       = Docente::URL;
   include '../../' . Administrador::URL . "notificacion/notificacion.detalle.php";
 
