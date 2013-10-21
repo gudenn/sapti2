18c7a390a2aa7a9b58bb1874d04cdd96d0abc2b6
Sesai Quispe <sesaiquispe@gmail.com>
2013-10-20 23:04:52 -0400

    revicion lista modificado

--- docente/revision/loaddata.revision.lista.php
+++ docente/revision/loaddata.revision.lista.php
@@ -5,7 +5,7 @@ include '../../_inc/_configurar.php';
 require_once('../EditableGrid.php');
 
 if(isset($_GET['doc'])){
-$proyecto=$_GET['doc'];
+$estuid=$_GET['doc'];
 };
          
 // Database connection
@@ -27,21 +27,24 @@ $grid->addColumn('action', 'Opciones', 'html', NULL, false);
 
 $result = $mysqli->query('
 SELECT re.id as id, re.estado as tipo, re.revisor_tipo as revtipo, re.fecha_revision as fecha, COUNT(ob.revision_id) as num, re.estado_revision as estado, re.fecha_aprobacion as correccion
-FROM proyecto pr, revision re, observacion ob
-WHERE pr.id="'.$proyecto.'"
+FROM proyecto pr, revision re, observacion ob, proyecto_estudiante pe
+WHERE pr.id=pe.proyecto_id
+AND pe.estudiante_id="'.$estuid.'"
 AND re.proyecto_id=pr.id
 AND re.id=ob.revision_id
 GROUP BY ob.revision_id
 UNION
 SELECT av.id as id, av.estado_avance as tipo, re.revisor_tipo as revtipo, av.fecha_avance as fecha, av.descripcion as num, av.estado_avance as estado, re.fecha_correccion as correccion
-FROM proyecto pr, avance av, revision re
-WHERE pr.id="'.$proyecto.'"
+FROM proyecto pr, avance av, revision re, proyecto_estudiante pe
+WHERE pr.id=pe.proyecto_id
+AND pe.estudiante_id="'.$estuid.'"
 AND av.proyecto_id=pr.id
 AND av.revision_id=re.id
 UNION
 SELECT av.id as id, av.revision_id as tipo, av.revision_id as revtipo, av.fecha_avance as fecha, av.descripcion as num, av.estado_avance as estado, av.fecha_avance as correccion
-FROM proyecto pr, avance av
-WHERE pr.id="'.$proyecto.'"
+FROM proyecto pr, avance av, proyecto_estudiante pe
+WHERE pr.id=pe.proyecto_id
+AND pe.estudiante_id="'.$estuid.'"
 AND av.revision_id=0
 AND av.proyecto_id=pr.id
 ');
