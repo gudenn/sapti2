d7c4ec68ebba14b301b57feac87760098ce820c2
ariel <ariel_nb_011@hotmail.com>
2013-10-23 19:10:57 +0200

    corregir acentos y demas

--- autoridad/reprogramacion/estado.gestion.php
+++ autoridad/reprogramacion/estado.gestion.php
@@ -27,7 +27,7 @@ try {
    * Menu superior
    */
   $menuList[]     = array('url'=>URL . Administrador::URL , 'name'=>'Administrador');
-  $menuList[]     = array('url'=>URL . Administrador::URL . 'reprogramacion/','name'=>'Reprogarmaci&oacute;n');
+  $menuList[]     = array('url'=>URL . Administrador::URL . 'reprogramacion/','name'=>'Reprogramaci&oacute;n');
   $menuList[]     = array('url'=>URL . Administrador::URL . 'reprogramacion/'.basename(__FILE__),'name'=>'Gesti&oacute;n de Estados');
   $smarty->assign("menuList", $menuList);
 
