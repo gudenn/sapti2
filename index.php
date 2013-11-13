23541fce28124ebdd3769c3ba9d14f8637e63a4f
Alavi <alaviserafin@gmail.com>
2013-11-13 09:46:02 -0400

    correcciones de los revisiones de los tribunales

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ try {
 from   usuario  u, estudiante  e, proyecto_estudiante  pe , proyecto p , defensa  d , lugar l
 where   u.id=e.usuario_id and e.id=pe.estudiante_id  and pe.proyecto_id=p.id
 and p.id=d.proyecto_id and d.tipo_defensa='DPU' and p.estado_proyecto='LD'
-and d.lugar_id= l.id  and d.fecha_defensa>= NOW()
+and d.lugar_id= l.id  
 ORDER  by   d.fecha_defensa  DESC;";
  $resultado = mysql_query($sqlr);
  $arraydefensa= array();
