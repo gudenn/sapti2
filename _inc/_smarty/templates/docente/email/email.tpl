ce9bddcbb32979789684705f59a7b5840721e910
Alavi <alavifuentes@hotmail.com>
2014-08-12 15:47:58 -0700

    email envios

---
+++ _inc/_smarty/templates/docente/email/email.tpl
@@ -0,0 +1,153 @@
+{include file="docente/header-sjq.tpl"}
+<div class="wrapper row3">
+  <div class="rnd">
+    <div id="container" class="clear">
+      <!-- ############ -->
+      {include file="docente/columna.left.tpl"}
+      <!-- ############ -->
+    
+<div id="content">
+   <center> <strong> <h1>Formulario De Envio de Email </h1></strong></td></center>
+      
+<div>
+    
+ <form action="" method="post" id="consejo" >
+               
+<div style='height:auto; width: 100%; font-size: 12px; overflow: auto;'>
+   
+ <form action="#" method="post" id="aprobado" name="aprobado" >
+
+   <table class="tbl_lista" id="docentes"  mane="docentes">
+  <thead>
+    <tr>
+    <th><a >ID          </a></th>
+    <th><a >NOMBRE      </a></th>
+    <th><a  >APELLIDOS     </a></th>
+    <th><a >SELECCIONAR</a></th>
+     </tr>
+  </thead>
+  <tbody>
+  <tr class="{cycle values="light,dark"}">
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>Marcar/Desmarcar Todo<input type="checkbox" id="marcar" value="" onclick="marcar_desmarcar();" /></td> 
+    </tr>
+    
+  {section name=ic loop=$listadocentes}
+    
+    <tr  class="selectable" id="{$listadocentes[ic][0]}" >
+   
+      <td>{$listadocentes[ic][0]}
+        <input type="hidden" name="ids[]" value="{$listadocentes[ic][0]}">
+      </td>
+      <td>
+        {$listadocentes[ic][1]}
+      </td>
+      <td>{$listadocentes[ic][2]}</td>
+        <td> 
+            <input type="checkbox" name=seleccion[] value={$listadocentes[ic][0]} class="checkbox" ></td> 
+ 
+    </tr>
+   {/section}
+
+    </tbody> 
+</table>
+     </div>
+        <div style ="clear"></div>
+       <div>
+       
+      <div>
+        Mensaje<br/>
+        <textarea name="detalle" rows="5" style="width: 90%">
+
+        </textarea>
+        <script>
+          CKEDITOR.replace('detalle'{$editores});
+        </script>
+      </div>
+     
+      
+      <div style="text-align: center">
+              <input type="hidden" name="tarea" value="registrar">
+              <input type="hidden" name="token" value="{$token}">
+
+              <input name="submit" type="submit" id="submit" value="Guardar">
+        
+     </div>
+   </div>
+ </form>
+
+    </div>
+<script type="text/javascript">
+
+function marcar_desmarcar(){
+var marca = document.getElementById('marcar');
+var cb = document.getElementsByName('seleccion[]');
+ 
+for (i=0; i<cb.length; i++){
+if(marca.checked == true){
+cb[i].checked = true
+}else{
+cb[i].checked = false;
+}
+}
+}
+
+</script>
+<script type="text/javascript">
+
+
+
+  jQuery(function(){
+    $("#docentes tbody").on("click", "tr", function(event){
+ if ($('#asignados > tbody >tr').length==3)
+    {
+     alert ( "Solo se Permiten tres Docentes como Tribunales!!" );
+      } else
+        {
+      var  trid = $(this).attr('id'); // table row ID 
+      
+        var fila=     $(this).html()
+                                       
+                             $.ajax({
+				url: 'verificar.php',
+				type: "GET",
+				data: "id="+trid,
+				success: function(datos){
+                               
+                               if(datos=="1")
+                               {
+                                    $("#asignados").append('<tr>' +fila + '</tr>');
+                                        $("#"+trid).remove(); 
+                               }else
+                               {
+                                   alert(datos)
+                               }
+                                        
+           
+                             }
+			});              
+       
+        
+        }
+        return false;
+      
+      
+     
+      
+    
+    
+    });
+  });
+</script>
+
+
+      </div>     
+        <p>{$ERROR}</p>
+  
+
+    </div>
+  </div>
+</div>
+{include file="footer.tpl"}
--- docente/email/index.php
+++ docente/email/index.php
@@ -1,48 +1,198 @@
 <?php
 try {
-  define ("MODULO", "DOCENTE");
+    define ("MODULO", "DOCENTE");
+  
   require('../_start.php');
   if(!isDocenteSession())
-    header("Location: ../login.php"); 
-
-  leerClase('Docente');
-  leerClase('Dicta');
-  $ERROR = '';
-
+  header("Location: login.php"); 
   /** HEADER */
-  $smarty->assign('title','Lista de Estudiantes');
-  $smarty->assign('description','Lista de Incritos a la Materia');
-  $smarty->assign('keywords','Gestion,Estudiantes');
+  $smarty->assign('title','Proyecto Final');
+  $smarty->assign('description','Proyecto Final');
+  $smarty->assign('keywords','Proyecto Final');
+  $CSS[]  = URL_JS . "ui/overcast/jquery-ui.css";
+  $CSS[]  = URL_JS . "jQfu/css/jquery.fileupload-ui.css";
+  $CSS[]  = URL_CSS . "academic/3_column.css";
+  $CSS[]  = URL_CSS . "spams.css";
+  $CSS[]  = URL_JS  . "validate/validationEngine.jquery.css";
 
-  //CSS
-  $CSS[]  = URL_CSS . "academic/tables.css";
-  $CSS[]  = URL_CSS . "editablegrid.css";
-  $CSS[]  = URL_CSS . "dashboardtabla.css";
-  $smarty->assign('CSS',$CSS);
-
-  //JS
+  
+   
+  
   $JS[]  = URL_JS . "jquery.min.js";
-  $JS[]  = URL_JS . "tablaeditable/editablegrid-2.0.1.js";
-  $JS[]  = URL_JS . "tablaeditable/tabla.estudiante.lista.js";
+  $JS[]  = URL_JS . "validate/idiomas/jquery.validationEngine-es.js";
+  $JS[]  = URL_JS . "validate/jquery.validationEngine.js";
+  $JS[]  = URL_JS . "validate/jquery.validate.min.js";
+
+    
+   $CSS[]  = URL_JS . "box/box.css";
+   $JS[]  = URL_JS ."box/jquery.box.js";
+  //CK Editor
+  $JS[]  = URL_JS . "ckeditor/ckeditor.js";
+  //BOX
+  $smarty->assign('CSS',$CSS);
   $smarty->assign('JS',$JS);
-  $docente     = getSessionDocente();
- 
-  $dicta=new Dicta($iddicta);
-   /**
+
+  //Leendo las clases para 
+  leerClase('Tribunal');
+  leerClase("Proyecto");
+  leerClase("Usuario");
+  leerClase("Docente");
+  leerClase("Estudiante");
+  leerClase("Formulario");
+  leerClase("Pagination");
+  leerClase("Filtro");
+  leerClase("Proyecto_estudiante");
+  leerClase("Notificacion");
+  leerClase("Notificacion_tribunal");
+  leerClase("Automatico");
+  leerClase("Consejo");
+  leerClase("Semestre");
+  
+  leerClase("Dia");
+  leerClase('Html');
+  
+ //  no hay error
+  
+  $smarty->assign("ERROR", '');
+    /**
    * Menu superior
    */
   $menuList[]     = array('url'=>URL.Docente::URL,'name'=>'Asignaturas');
-   $menuList[]     = array('url'=>URL.Docente::URL.'email','name'=>'Envio a Email');
+  $menuList[]     = array('url'=>URL . Docente::URL.'email' ,'name'=>'Envio de Email');
   $smarty->assign("menuList", $menuList);
+ 
+   $editores = ",
+                {toolbar: [ 
+                  [ 'Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink' ],
+                  [ 'FontSize', 'TextColor', 'BGColor' ]]}";
+  
+    $smarty->assign("editores", $editores);
+    $diass= new Dia();
+    $smarty->assign("diass", $diass);
+    $html = new Html();
+    
+     $semestre = new Semestre('',1);
+    
+         /**
+   * Menu central
+   */
+  $docente = getSessionDocente(); 
+  leerClase('Menu');
+  $menu = new Menu('');
+  $menus = $menu->getDocenteIndex($docente);
+  $smarty->assign("menus", $menus);
+  $smarty->assign("docente", $docente);
+  $smarty->assign("ERROR", $ERROR);
+     
+     
+    $valorh = $semestre->getValor('Número máximo de tribunal',10);
+    if (!$valorh)
+    {
+      //  echo $valorh;
+       $semestre->setValor('Número máximo de tribunal',10);
+    }
 
-  $smarty->assign("docente",  $docente );
+    
+   
+ $sqlr='SELECT es.id as id, es.codigo_sis as codigosis, us.nombre as nombre, CONCAT(us.apellido_paterno," ",us.apellido_materno) as apellidos, pr.nombre as nombrep
+FROM dicta di, estudiante es, usuario us, inscrito it, proyecto pr, proyecto_estudiante pe
+WHERE di.id=it.dicta_id
+AND it.estudiante_id=es.id
+AND es.usuario_id=us.id
+AND pe.estudiante_id=es.id
+AND pe.proyecto_id=pr.id
+AND pr.es_actual=1
+AND di.docente_id="'.$docente->id.'"';
+ $resultado = mysql_query($sqlr);
+ $arraytribunal= array();
 
-  //No hay ERROR
-  $smarty->assign("ERROR",$ERROR);
-}
+ while ($fila = mysql_fetch_array($resultado, MYSQL_ASSOC)) 
+   { 
+       $doc= new Docente($fila["id"]);
+        $listaareas=array();
+        $lista_areas=array();
+        $lista_areas[] =  $fila["id"];
+        $lista_areas[] =  $fila["nombre"];
+        $lista_areas[] =  $fila["apellidos"];
+            
+  $arraytribunal[]= $lista_areas;
+  
+ }
+  $smarty->assign('listadocentes'  , $arraytribunal);
+  $contenido = 'tribunal/registrotribunal.tpl';
+  $smarty->assign('contenido',$contenido);
+  
+
+
+if(isset($_POST['tarea']) && $_POST['tarea'] == 'registrar' && isset($_POST['token']) && $_SESSION['register'] == $_POST['token'])
+ {
+    
+        if (isset($_POST['seleccion'])) 
+           {
+            $seleccion=$_POST['seleccion'];
+           if(count($seleccion)>0){
+           foreach ($seleccion as $id){
+               
+           }
+               
+      
+   
+     $EXITO = false;
+    $stado=0;
+    mysql_query("BEGIN");
+    
+    
+     
+    $EXITO = TRUE;
+    $stado=1;
+    mysql_query("COMMIT");
+    
+  if(isset($stado))
+   {
+    if($stado==1)
+      {
+       $_SESSION['estado']=$stado;
+       header("Location: listatribunal.php");
+          
+
+      }else
+        
+        {
+          $mensaje = array('mensaje'=>'Hubo un problema, No se grabo correctamente la asignacion de tribunales','titulo'=>'Registro De Asignaci&oacute; de Tribunales' ,'icono'=> 'warning_48.png');
+          $ERROR = $html->getMessageBox ($mensaje);
+       }
+    }
+    
+    
+        
+     }else
+ {
+   $mensaje = array('mensaje' => 'Error,La Cantidad minima de Tribunales debe Ser 3', 'titulo' => 'Numero De Tribunales', 'icono' => 'warning_48.png');
+    $ERROR = $html->getMessageBox($mensaje);;
+ }
+ }else
+ {
+     $mensaje = array('mensaje' => 'Hubo un problema, No se grabo correctamente la Asignacion de Tribunales', 'titulo' => 'Registro de Tribunales', 'icono' => 'warning_48.png');
+    $ERROR = $html->getMessageBox($mensaje);
+ }
+ 
+ }
+ 
+
+
+  $smarty->assign("ERROR",  $ERROR);
+   
+} 
 catch(Exception $e) 
 {
+   
   $smarty->assign("ERROR", handleError($e));
 }
-  $smarty->display('docente/email/full-width.estudiante.lista.tpl');
+$token = sha1(URL . time());
+$_SESSION['register'] = $token;
+$smarty->assign('token', $token);
+
+$TEMPLATE_TOSHOW = 'docente/email/email.tpl';
+$smarty->display($TEMPLATE_TOSHOW);
+
 ?>
