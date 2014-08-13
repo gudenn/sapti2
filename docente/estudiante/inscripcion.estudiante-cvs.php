1fbdb56dba21a5820ce7e52eef72f83e4bc3c2cc
Sesai Quispe <sesaiquispe@gmail.com>
2014-08-13 18:34:26 -0400

    Importar de CSV

--- docente/estudiante/inscripcion.estudiante-cvs.php
+++ docente/estudiante/inscripcion.estudiante-cvs.php
@@ -105,6 +105,89 @@ AND es.codigo_sis='$sis'";
   if (isset($_POST['tarea']) && $_POST['tarea'] == 'registrar'  && isset($_POST['token']) && $_SESSION['register'] == $_POST['token'])
   {
     mysql_query("BEGIN");
+		$filename=$_FILES["file"]["tmp_name"];
+		 if($_FILES["file"]["size"] > 0)
+		 {
+		  	$file = fopen($filename, "r");
+	         while (($estudiantes = fgetcsv($file, 10000, ",")) !== FALSE){
+    $estudiantesaux=$estudiantes;
+    if (count($estudiantes)>=1)
+      foreach ($estudiantes as $estudiante_array) {
+        $estudiante_array = explode(';', $estudiante_array);
+        if (count($estudiante_array)>=1 && is_numeric($estudiante_array[1]) )
+        {
+                  $estudiante = new Estudiante();
+          $estudiante->getByCodigoSis($estudiante_array[1]);
+            if ($estudiante->id){
+                if(estainscrito($estudiante_array[1], $iddicta)=='0'){
+                    $estudiante->inscribirEstudianteDicta($semestre->id, $iddicta);
+                    $proyid=$estudiante->getProyecto();
+                    $proyid->asignarDictaest($iddicta);
+                    $proyid->save();
+                    $inscritos[]=$estudiante_array;
+                    }else{
+                    $yainscritos[]=$estudiante_array;
+                    }
+            }else{
+                        $usuario = new Usuario();
+                        $usuario->objBuidFromPost();
+                        $estudiante->objBuidFromPost();
+                        $usuario->parserNombreApellidos($estudiante_array[2]); //Nombres y apellidos
+                        $usuario->estado    = Objectbase::STATUS_AC;
+                        $usuario->login     = $estudiante_array[1];            //codigo sis
+                        $usuario->clave     = $estudiante_array[1]; 
+                        $usuario->estado = Objectbase::STATUS_AC;
+                        $usuario->save();
+
+                        //usuario pertenece a un grupo
+                        $usuario->asignarGrupo(Grupo::GR_ES);
+
+                        $estudiante->estado     = Objectbase::STATUS_AC;
+                        $estudiante->codigo_sis = $estudiante_array[1];
+                        $estudiante->usuario_id = $usuario->id;
+                        $estudiante->save();
+
+                          $materia = new Materia($dicta->materia_id);
+                          $estudiante->crearProyectoInicial($iddicta, $materia->tipo);
+                          $estudiante->inscribirEstudianteDicta($semestre->id, $iddicta);
+               $noestudiante[]=$estudiante_array;
+                 }
+        }
+      }
+      if (isset($_POST['listaoficial'])) 
+        $listaoficial=$_POST['listaoficial'];
+        $smarty->assign("listaoficial", $listaoficial);
+
+      if($listaoficial[0]=="borrar"){
+              $listainscritos = "SELECT it.id as id, es.codigo_sis as sis
+                    FROM inscrito it, estudiante es
+                    WHERE it.estudiante_id=es.id
+                    AND it.dicta_id='$iddicta'
+                    AND it.semestre_id='$semestre->id'";
+                  $resultins = mysql_query($listainscritos);
+                  while ($rowins = mysql_fetch_array($resultins, MYSQL_ASSOC)) {
+                       $listainsactu[] = $rowins;
+                  }
+                 if(count($listainsactu)>0 && count($listainsactu)>count($estudiantesaux)){
+                  foreach ($estudiantesaux as $estaux) {
+                     $estaux = explode(';', $estaux);
+                     $listainsactu=array_values($listainsactu);
+                      for ($i=0; $i<=count($listainsactu); $i++){
+                          if ($estaux[1]==$listainsactu[$i]['sis']){
+                              unset($listainsactu[$i]);
+                          }  
+                      }
+                  }
+                  foreach ($listainsactu as $ins){
+                      $borrarins=new Inscrito($ins['id']);
+                      $borrarins->borrarInscrito();
+                  }
+                  }
+      }
+	         }
+	         fclose($file);
+                 }
+    /*             
     $aux      = str_replace(array("\r\n", "\r", "\n"), '#CODIGOX#', trim($_POST['cvs'])); 
     $estudiantes = explode('#CODIGOX#', $aux);
     $estudiantesaux=$estudiantes;
@@ -181,6 +264,8 @@ AND es.codigo_sis='$sis'";
                   }
                   }
       }
+     * 
+     */
       $yainscritos=array_envia($yainscritos);
       $inscritos=array_envia($inscritos);
       $noestudiante=array_envia($noestudiante);
