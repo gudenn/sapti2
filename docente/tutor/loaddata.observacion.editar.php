33417455c6cbaa8bb41a2cf2e441ee2cf21bacd0
Sesai Quispe <sesaiquispe@gmail.com>
2015-10-15 14:46:19 -0600

    Correccion observaciones pendientes en tribunal y tutor

---
+++ docente/tutor/loaddata.observacion.editar.php
@@ -0,0 +1,32 @@
+<?php     
+define ("MODULO", "DOCENTE");
+require  '../_start.php';
+include '../../_inc/_configurar.php';       
+require_once('../EditableGrid.php');
+
+if(isset($_GET['revid'])){
+$obser2=$_GET['revid'];
+};
+
+// Database connection
+$mysqli = mysqli_init();
+$mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5);
+$mysqli->real_connect(DBHOST,DBUSER,BDPASS,BDNAME);
+                    
+// create a new EditableGrid object
+$grid = new EditableGrid();
+
+$grid->addColumn('id', 'ID', 'integer', NULL, false); 
+$grid->addColumn('observacion', 'Observación', 'string');
+$grid->addColumn('action', 'Borrar', 'html', NULL, false);
+
+$result = $mysqli->query('
+SELECT ob.id AS id, ob.observacion AS observacion
+FROM observacion ob
+WHERE revision_id="'.$obser2.'"');
+
+$mysqli->close();
+
+$grid->renderXML($result);
+
+?>
